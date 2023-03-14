d61c36d10ec6139d0c37e0a0f4e62eac5d05d464
Addin Munawwar <moonawar@protonmail.com>
2023-03-15 01:12:07 +0700

    feat: interrupt (tested done)

--- src/kernel.c
+++ src/kernel.c
@@ -4,14 +4,28 @@
 #include "lib-header/gdt.h"
 #include "lib-header/framebuffer.h"
 #include "lib-header/kernel_loader.h"
+#include "lib-header/idt.h"
+#include "lib-header/interrupt.h"
 
 void kernel_setup(void) {
+    /* Initialization */
     enter_protected_mode(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
     framebuffer_clear();
-    framebuffer_write(3, 8, 'H', 0, 0xF);
-    framebuffer_write(3, 9, 'a', 0, 0xF);
-    framebuffer_write(3, 10, 'i', 0, 0xF);
-    framebuffer_write(3, 11, '!', 0, 0xF);
-    framebuffer_set_cursor(3, 10);
+
+    /* Write to framebuffer */
+    framebuffer_write(3, 9, ' ', 0, 0xF);
+    framebuffer_write(3, 14, ' ', 0, 0xF);
+    framebuffer_write(5, 9, ' ', 0, 0xF);
+    framebuffer_write(6, 10, ' ', 0, 0xF);
+    framebuffer_write(6, 11, ' ', 0, 0xF);
+    framebuffer_write(6, 12, ' ', 0, 0xF);
+    framebuffer_write(6, 13, ' ', 0, 0xF);
+    framebuffer_write(5, 14, ' ', 0, 0xF);
+    framebuffer_set_cursor(3, 9);
+
+    /* Test interrupt */
+    __asm__("int $0x4");
     while (TRUE);
 }
