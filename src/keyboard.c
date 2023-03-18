63e855a3436cfa8d17d30663ad21f7b626265d6b
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2023-03-19 02:07:51 +0700

    remove: redundant documentation + TODO

--- src/keyboard.c
+++ src/keyboard.c
@@ -32,50 +32,38 @@ static struct KeyboardDriverState keyboard_state = {
 
 static int row = 0;
 
-/* -- Driver Interfaces -- */
-
-// Activate keyboard ISR / start listen keyboard & save to buffer
 void keyboard_state_activate(void){
     activate_keyboard_interrupt();
     keyboard_state.keyboard_input_on = TRUE;  
 }
 
-// Deactivate keyboard ISR / stop listening keyboard interrupt
 void keyboard_state_deactivate(void){
     keyboard_state.keyboard_input_on = FALSE;
     keyboard_state.buffer_index      = 0;
     memset(keyboard_state.keyboard_buffer, 0, KEYBOARD_BUFFER_SIZE);
 }
 
-// Get keyboard buffer values - @param buf Pointer to char buffer, recommended size at least KEYBOARD_BUFFER_SIZE
 void get_keyboard_buffer(char *buf){
     memcpy(buf, keyboard_state.keyboard_buffer, KEYBOARD_BUFFER_SIZE);
 }
 
-// Check whether keyboard ISR is active or not - @return Equal with keyboard_input_on value
 bool is_keyboard_blocking(void){
     return keyboard_state.keyboard_input_on;
 }
 
-/* -- Keyboard Interrupt Service Routine -- */
-
-/**
- * Handling keyboard interrupt & process scancodes into ASCII character.
- * Will start listen and process keyboard scancode if keyboard_input_on.
- * 
- * Will only print printable character into framebuffer.
- * Stop processing when enter key (line feed) is pressed.
- * 
- * Note that, with keyboard interrupt & ISR, keyboard reading is non-blocking.
- * This can be made into blocking input with `while (is_keyboard_blocking());` 
- * after calling `keyboard_state_activate();`
- */
 void keyboard_isr(void){
     if (!keyboard_state.keyboard_input_on)
         keyboard_state.buffer_index = 0;
     else {
         uint8_t  scancode    = in(KEYBOARD_DATA_PORT);
         char     mapped_char = keyboard_scancode_1_to_ascii_map[scancode];
+        
+        /**
+         * TODO:
+         * - handle extended scancode
+         * - change keyboard buffer clearance -> should not be cleared
+         * - handle multi row input with framebuffer_get_cursor()
+        */
 
         if (scancode & 0x80) { //ignore release interrupt
             // .. ignore
